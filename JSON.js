bc6009b01a649bec898057480f900ddbb53df622
KSArjun18 <111218912+KSArjun18@users.noreply.github.com>
2022-08-15 19:43:20 +0530

    Add files via upload

---
+++ JSON.js
@@ -0,0 +1,31 @@
+
+var json = [{
+    "id" : "Arjun",
+    "msg"   : "Day 3 task",
+    "task" : "zen portal task",
+    "mail": "arjunkishore18@gmail.com",
+},
+
+];
+for(var i = 0; i < json.length; i++) {
+    var obj = json[i];
+
+    console.log(obj.id);
+    console.log(obj.msg);
+    console.log(obj.task);
+    console.log(obj.mail);
+
+}
+json.forEach(function(obj) { console.log(obj.msg); });
+
+for (var key in json) {
+if (json.hasOwnProperty(key)) {
+  console.log(json[key].id);
+ 
+}
+}
+let text = "";
+for (let x of json[key].id) {
+ text += x; 
+}
+ console.log(text);
