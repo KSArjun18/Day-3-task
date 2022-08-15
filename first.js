25c2d86945b6cde678406183fe52719ce13f9eb4
KSArjun18 <111218912+KSArjun18@users.noreply.github.com>
2022-08-15 19:35:30 +0530

    Add files via upload

--- first.js
+++ first.js
@@ -1,30 +1,15 @@
+var Resume ={
+    Name : "Arjun",
+    "Phone number": 9600765116,
+    Email : "arjunkishore18@gamil.com",
+    Adderss: "253/11th cross T.N.H.B , Walajapet",
+    Qualification:"Bsc.Computer science",
+    Skills: "Coding, Hacking , ",
+    "Date of brith" :"18.04.2001", 
+    "Father name": "V kumar",
+    "Mother name": "Sasikala",
+   
+    }
+    console.log(Resume);
+    console.log(JSON.stringify(Resume));
 
-var json = [{
-    "id" : "Arjun",
-    "msg"   : "Day 3 task",
-    "task" : "zen task",
-    "mail": "arjunkishore18@gmail.com",
-},
-];
-for(var i = 0; i < json.length; i++) 
-{
-    var obj = json[i];
-    console.log(obj.id);
-    console.log(obj.msg);
-    console.log(obj.task);
-    console.log(obj.mail);
-}
-json.forEach(function(obj) 
-{ console.log(obj.msg); });
-for (var key in json) {
-if (json.hasOwnProperty(key)) 
-{
-  console.log(json[key].id);
-}
-}
-let text = "";
-for (let x of json[key].id)
- {
- text += x; 
-}
- console.log(text);
